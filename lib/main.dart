d9d8c575242f4999aa1c55495dad19c8232b0366
Abdulkareeem Alramlawi <abdulkarim.k.r11@gmail.com>
2022-04-06 16:32:01 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(FlashChat());
+  
 }
 
 class FlashChat extends StatelessWidget {
